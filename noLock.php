caf88f1339929dd3c0440cd16f396ca4febfcb4c
Mrxxm <362190221@qq.com>
2020-11-27 12:04:01 +0800

    提交

---
+++ noLock.php
@@ -0,0 +1,18 @@
+<?php 
+
+$pdo = new PDO('mysql:host=127.0.0.1;dbname=test', 'root', '9511134231');
+$sql="select `number` from  storage where id=1 limit 1";
+$res = $pdo->query($sql)->fetch();
+$number = $res['number'];
+if($number>0)
+{
+    $sql ="insert into `order`  VALUES (null,$number)";
+
+    $order_id = $pdo->query($sql);
+    if($order_id)
+    {
+
+        $sql="update storage set `number`=`number`-1 WHERE id=1";
+        $pdo->query($sql);
+    }
+}
